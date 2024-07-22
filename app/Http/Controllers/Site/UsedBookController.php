e38dcf1e16c5c26e46197ea9fa735560c9d77928
DevZakariaElkashef <devzakariaelkashef@gmail.com>
2024-07-22 09:53:58 +0300

    add filter to used books

--- app/Http/Controllers/Site/UsedBookController.php
+++ app/Http/Controllers/Site/UsedBookController.php
@@ -12,9 +12,46 @@ use App\Models\Slider;
 
 class UsedBookController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $usedBooks = Book::active()->used()->get();
+        $usedBooks = Book::query();
+
+        if ($request->filled('sort')) {
+            if ($request->sort == 'latest') {
+                $usedBooks->latest();
+            } elseif ($request->sort == 'most-saled') {
+                $usedBooks = Book::getMostSoldBooks();
+            }
+        }
+
+        if ($request->filled('name')) {
+            $usedBooks->where(function ($query) use ($request) {
+                $query->where('name_ar', 'like', '%' . $request->name . '%')
+                    ->orWhere('name_en', 'like', '%' . $request->name . '%')
+                    ->orWhere('description_ar', 'like', '%' . $request->name . '%')
+                    ->orWhere('description_en', 'like', '%' . $request->name . '%');
+            });
+        }
+
+        if ($request->filled('university_id')) {
+            $usedBooks->whereHas('subject', function ($subject) use ($request) {
+                $subject->whereHas('college', function ($college) use ($request) {
+                    $college->where('university_id', $request->university_id);
+                });
+            });
+        }
+
+        if ($request->filled('college_id')) {
+            $usedBooks->whereHas('subject', function ($subject) use ($request) {
+                $subject->where('college_id', $request->college_id);
+            });
+        }
+
+        if ($request->filled('subject_id')) {
+            $usedBooks->where('subject_id', $request->subject_id);
+        }
+
+        $usedBooks = $usedBooks->active()->used()->paginate(9);
 
 
         $universities = University::active()->get();
